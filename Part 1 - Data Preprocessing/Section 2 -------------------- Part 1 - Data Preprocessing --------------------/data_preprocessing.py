b2411e592ee0de569c5b4a70e50c30dd91341300
nimesh12maheshwari <45997691+nimesh12maheshwari@users.noreply.github.com>
2020-01-14 12:59:12 +0530

    First

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 12:50:12 2020
+
+@author: Nimesh
+"""
+
+# IMporting the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset
